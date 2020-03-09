48eacb4214c5916f8bab90c5b704c7f5da345eef
Tahmid Hasan <developers.darkpixel@gmail.com>
2020-03-09 22:36:36 +0600

    Set Cookie Middleware

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 
 import routes from './routes'
-import { setCookie } from './middlewares'
 
 const app = express()
 
@@ -21,7 +20,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(setCookie)
 app.use(routes)
 
 export default app
